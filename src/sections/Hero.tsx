1b9e96e640363c7b73e5e1cb206b8d138c75d069
reynaroma <roma.reynamay@gmail.com>
2024-05-15 16:58:31 -0600

    Add style on the p element of the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,17 +9,17 @@ function Hero() {
     md:gap-0 lg:px-12 xl:m-auto xl:gap-0 xl:overflow-hidden">
       <div className="sm:w-full md:w-3/6">
         {/* LEFT SIDE */}
-        <h1 
-        className="text-center mx-auto mb-8 w-[12ch] text-4xl font-semibold text-gray-800 sm:text-5xl md:mx-0 md:text-left">Start spending the smart way</h1>
-        <p className="">Take Control of your Finances Anytime, Anywhere with Kobodrop.
+        <h1
+          className="text-center mx-auto mb-8 w-[12ch] text-4xl font-semibold text-gray-800 sm:text-5xl md:mx-0 md:text-left">Start spending the smart way</h1>
+        <p className="m-auto w-[34ch] text-center text-gray-500 md:m-0 md:text-left">Take Control of your Finances Anytime, Anywhere with Kobodrop.
           Discover the Smart Way to Use Your Money!
         </p>
         <StoreLinks />
       </div>
-      <div>
+      <div className="">
         {/* RIGHT SIDE */}
-        <img src={Phone} alt="Kobodrop App Frame"/>
-        <img src={HeroImage} alt="A woman happily using Kobodrop"/>
+        <img src={Phone} alt="Kobodrop App Frame" />
+        <img src={HeroImage} alt="A woman happily using Kobodrop" />
       </div>
     </section>
   )
