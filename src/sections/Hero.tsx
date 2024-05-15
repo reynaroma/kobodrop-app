2704f9a73d9ee563dd529a2790befb429c60f2bc
reynaroma <roma.reynamay@gmail.com>
2024-05-15 16:17:17 -0600

    Add hero images

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,23 @@
-import StoreLinks from "../common/StoreLinks"
+import StoreLinks from "../common/StoreLinks";
+import Phone from "../assets/app_frame.png";
+import HeroImage from "../assets/hero.png";
 
 function Hero() {
   return (
     <section>
       <div>
-        {/* LEFT */}
+        {/* LEFT SIDE */}
         <h1>Start spending the smart way</h1>
         <p>Take Control of your Finances Anytime, Anywhere with Kobodrop.
           Discover the Smart Way to Use Your Money!
         </p>
         <StoreLinks />
       </div>
-
+      <div>
+        {/* RIGHT SIDE */}
+        <img src={Phone} alt="Phone Image"/>
+        <img src={HeroImage} alt="Hero Image"/>
+      </div>
     </section>
   )
 }
