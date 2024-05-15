13ee167fe82ea30555ac2858d83237506bac9fc3
reynaroma <roma.reynamay@gmail.com>
2024-05-14 19:45:27 -0600

    Add a target to open a new window when Install Button clicked

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -8,14 +8,17 @@ function Nav() {
       {({ open }) => (
         <>
           <div className="flex h-[15vh] items-center max-w-7xl px-8">
-            <a href="#">
-              <img src={Kobodrop} alt="Kobodrop" />
-            </a>
-            <div className="flex space-x-4 items-center ml-4">
-              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#features">Features</a>
-              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#partners">Partners</a>
-              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#reviews">Reviews</a>
+            <div className="flex">
+              <a href="#">
+                <img src={Kobodrop} alt="Kobodrop" />
+              </a>
+              <div className="flex space-x-4 items-center ml-4">
+                <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#features">Features</a>
+                <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#partners">Partners</a>
+                <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#reviews">Reviews</a>
+              </div>
             </div>
+            <a href="https://play.google.com" target="_blank" className="">Install Kobodrop</a>
           </div>
         </>
       )
