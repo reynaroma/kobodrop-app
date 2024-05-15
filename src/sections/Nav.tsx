1619e4501ef327a0cba0c65e53ede3362fbd102d
reynaroma <roma.reynamay@gmail.com>
2024-05-14 19:17:33 -0600

    Add spaces and margins on the nav button links

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -7,17 +7,20 @@ function Nav() {
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="flex">
-            <a href="#"><img src={Kobodrop} alt="" /></a>
-            <div className="flex">
-              <div>ButtonLink</div>
-              <div>ButtonLink</div>
-              <div>ButtonLink</div>
+          <div className="flex h-[15vh] items-center max-w-7xl px-8">
+            <a href="#">
+              <img src={Kobodrop} alt="Kobodrop" />
+            </a>
+            <div className="flex space-x-4 items-center ml-4">
+              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#features">Features</a>
+              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#partners">Partners</a>
+              <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#reviews">Reviews</a>
             </div>
           </div>
         </>
-      )}
-    </Disclosure>
+      )
+      }
+    </Disclosure >
   )
 }
 
