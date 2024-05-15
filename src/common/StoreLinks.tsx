b98b7c54beb1f12d200d6237ebc214c3a06a0a58
reynaroma <roma.reynamay@gmail.com>
2024-05-15 15:59:21 -0600

    Add the imports from the Hero component to refactor the component

---
+++ src/common/StoreLinks.tsx
@@ -0,0 +1,13 @@
+import AppleLogo from '../assets/logos/app_store.svg';
+import GoogleLogo from '../assets/logos/google_play.svg';
+import StoreLink from '../common/StoreLink';
+
+function StoreLinks() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StoreLinks
