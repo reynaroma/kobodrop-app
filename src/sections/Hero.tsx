22e717c36ab7514f5261f47432c275df2edfd682
reynaroma <roma.reynamay@gmail.com>
2024-05-15 16:56:16 -0600

    Add style on the h1 of the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,10 +7,11 @@ function Hero() {
     <section className="relative mt-4 flex h-fit max-w-7xl flex-col
     items-center gap-10 px-8 sm:gap-16 md:my-0 md:flex-row md:h-[85vh]
     md:gap-0 lg:px-12 xl:m-auto xl:gap-0 xl:overflow-hidden">
-      <div>
+      <div className="sm:w-full md:w-3/6">
         {/* LEFT SIDE */}
-        <h1>Start spending the smart way</h1>
-        <p>Take Control of your Finances Anytime, Anywhere with Kobodrop.
+        <h1 
+        className="text-center mx-auto mb-8 w-[12ch] text-4xl font-semibold text-gray-800 sm:text-5xl md:mx-0 md:text-left">Start spending the smart way</h1>
+        <p className="">Take Control of your Finances Anytime, Anywhere with Kobodrop.
           Discover the Smart Way to Use Your Money!
         </p>
         <StoreLinks />
