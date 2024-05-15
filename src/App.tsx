d317c52f14fe17007026e1af1e82e7f2b65a5993
reynaroma <roma.reynamay@gmail.com>
2024-05-15 17:36:35 -0600

    Create article for the Features section and other elements then import it in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Nav from "./sections/Nav";
 import Hero from "./sections/Hero";
+import Features from "./sections/Features";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Nav />
       <Hero />
+      <Features />
     </>
   )
 }
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,9 +1,17 @@
 
 function Features() {
   return (
-    <div>
-
-    </div>
+    <section id="features"
+      className="flex max-w-7xl flex-col gap-10 px-10 pt-10 lg:px-12 xl:pt-20">
+      <article className="m-auto w-[30ch] text-center text-gray-500 md:m-0 md:w-full">
+        <h2 className="mb-4 text-4xl font-semibold text-gray-800">Make every penny count</h2>
+        <p>
+          Spend smarter, lower your bills, get cashback on everything you buy, <br />
+          and unlock credit to grow your business.
+        </p>
+        {/* <p>Paying your bills on Kobodrop has never been easier. Whether you are paying for electricity or internet, Kobodrop gets it done within seconds.</p> */}
+      </article>
+    </section>
   )
 }
 
