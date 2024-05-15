f7861b4cbba317e3b39c5ace881e4c9eb36ec815
reynaroma <roma.reynamay@gmail.com>
2024-05-15 16:00:57 -0600

    Copy the StoreLink to common file

--- src/common/StoreLinks.tsx
+++ src/common/StoreLinks.tsx
@@ -4,10 +4,27 @@ import StoreLink from '../common/StoreLink';
 
 function StoreLinks() {
   return (
-    <div>
-      
+    <div className='mt-10 justify-center space-x-2 sm:flex md:justify-normal'>
+      <StoreLink
+        href="https://www.apple.com/app-store"
+        upperText='Download on the'
+        lowerText='Apple Store'
+        logo={AppleLogo}
+        className='flex gap-3 bg-zinc-900 px-4 py-3 rounded
+          text-white hover:bg-zinc-950 active:bg-zinc-800'
+        target='_blank'
+      />
+      <StoreLink
+        href="https://play.google.com/"
+        upperText='Get it on'
+        lowerText='Google Play'
+        logo={GoogleLogo}
+        className='flex gap-3 bg-zinc-900 px-4 py-3 rounded
+          text-white hover:bg-zinc-950 active:bg-zinc-800'
+        target='_blank'
+      />
     </div>
   )
 }
 
-export default StoreLinks
+export default StoreLinks;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,3 @@
-import AppleLogo from '../assets/logos/app_store.svg';
-import GoogleLogo from '../assets/logos/google_play.svg';
-import StoreLink from '../common/StoreLink';
 
 function Hero() {
   return (
@@ -12,26 +9,7 @@ function Hero() {
           Discover the Smart Way to Use Your Money!
         </p>
       </div>
-      <div className='mt-10 justify-center space-x-2 sm:flex md:justify-normal'>
-        <StoreLink
-          href="https://www.apple.com/app-store"
-          upperText='Download on the'
-          lowerText='Apple Store'
-          logo={AppleLogo}
-          className='flex gap-3 bg-zinc-900 px-4 py-3 rounded
-          text-white hover:bg-zinc-950 active:bg-zinc-800'
-          target='_blank'
-        />
-        <StoreLink
-          href="https://play.google.com/"
-          upperText='Get it on'
-          lowerText='Google Play'
-          logo={GoogleLogo}
-          className='flex gap-3 bg-zinc-900 px-4 py-3 rounded
-          text-white hover:bg-zinc-950 active:bg-zinc-800'
-          target='_blank'
-        />
-      </div>
+      
 
     </section>
   )
