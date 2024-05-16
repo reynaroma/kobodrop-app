13e2fb81a9be1644c6035cc8ae0bf0f43466283d
reynaroma <roma.reynamay@gmail.com>
2024-05-15 20:50:07 -0600

    Add new article by copying it

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -36,6 +36,28 @@ function Features() {
           </p>
         </div>
       </article>
+      <article className="flex w-full flex-col gap-8 overflow-hidden xl:h-96 xl:flex-row">
+        <div className="flex flex-col rounded-2xl bg-sky-100 px-4 sm:px-0 md:flex-row md:gap-8 xl:w-2/3">
+          <div className="mt-10 flex flex-col justify-center gap-4 text-center sm:mx-10 md:mx-0 md:ml-10 md:w-1/2 md:text-left">
+            <h2 className="m-auto text-center text-3xl font-semibold text-gray-800 sm:w-[18ch] md:m-0 md:text-left">Pay with Kobodrop, quick, simple and easy</h2>
+            <p className="m-auto text-center text-gray-500 sm:w-[34ch] md:m-0 md:text-left">
+              Paying your bills on Kobodrop has never been easier. Whether you are paying for electricity or internet, Kobodrop gets it done within seconds.
+            </p>
+          </div>
+          <div className='m-auto mt-10 max-w-72 md:mx-10 md:w-1/2 lg:mx-0'>
+            <img src={AppFrame} alt='Kobodrop app frame' />
+          </div>
+        </div>
+        <div className='flex flex-col justify-center gap-4 rounded-2xl bg-indigo-100 p-10 xl:w-1/3'>
+          <div className='w-fit rounded-full bg-indigo-200 p-4'>
+            <img src={ShieldCheck} alt='' />
+          </div>
+          <h2 className='text-3xl font-semibold text-gray-800'>Bank level security</h2>
+          <p className='text-gray-500'>
+            Your money is 100% safe and secure on Kobodrop. No hassles, no glitches, get access to your money anytime.
+          </p>
+        </div>
+      </article>
     </section>
   )
 }
