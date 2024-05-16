2ca90a431500aed4502253176a206c84f39a216c
reynaroma <roma.reynamay@gmail.com>
2024-05-15 21:32:29 -0600

    Add elements on the Partners section and style it

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -1,8 +1,23 @@
+import Klarna from '../assets/logo-wall/klarna.png';
+import Stripe from '../assets/logo-wall/stripe.png';
+import Paypal from '../assets/logo-wall/paypal.png';
+
 function Partners() {
   return (
-    <div>
-      
-    </div>
+    <section id="partners" className="mt-20 flex flex-col bg-gray-50 py-20">
+      <article className="mb-10 w-full px-8 text-center lg:px-12">
+        <h2 className="mb-4 text-4xl font-semibold text-gray-800">Transact seamless with...</h2>
+        <p className="">Connect your other accounts to Kobodrop seamlessly. Kobodrop supports 200+ <br />
+          integrations with other payment platforms like stripe, paypal, payoneer and others</p>
+      </article>
+      <div className='xl:flex xl:justify-center'>
+        <article className='flex mb-8 justify-center gap-8 text-center'>
+          <img src={Klarna} alt="Klarna logo" className='w-20 md:w-36' />
+          <img src={Stripe} alt="Stripe logo" className='w-20 md:w-36' />
+          <img src={Paypal} alt="Paypal logo" className='w-20 md:w-36' />
+        </article>
+      </div>
+    </section>
   )
 }
 
