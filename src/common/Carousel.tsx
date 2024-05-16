937d6c712cb25f2a4e688cc6c112743c626432f2
reynaroma <roma.reynamay@gmail.com>
2024-05-15 22:49:31 -0600

    Add transition on the slide component

--- src/common/Carousel.tsx
+++ src/common/Carousel.tsx
@@ -1,23 +1,26 @@
+import { useState } from "react";
 interface CarouselProps {
   slides: Slide[];
 }
 
-interface Slide { 
+interface Slide {
   src: string;
   text: string;
   name: string;
   country: string;
 }
 
-function Carousel({slides}: CarouselProps) {
-  return (
-    <div>
-      {slides.map((slide, index) => (
-        <div key={index}>
-          {slide.name}
-        </div>
-      ))}<div>
+function Carousel({ slides }: CarouselProps) {
+  let [current, setCurrent] = useState(0);
 
+  return (
+    <div className="relative h-[400px] max-w-[400px] overflow-hidden rounded-2xl">
+      <div className="flex transition duration-300 ease-out" style={{transform: `translateX(-${current * 100}%)`}}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            {slide.name}
+          </div>
+        ))}
       </div>
     </div>
   )
