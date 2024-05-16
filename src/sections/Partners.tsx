866b3e9ed4a49d59de020576a9e8805dc753e3da
reynaroma <roma.reynamay@gmail.com>
2024-05-15 21:47:55 -0600

    Finish setup and reuse the common component for the partners section

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -1,6 +1,15 @@
 import Klarna from '../assets/logo-wall/klarna.png';
 import Stripe from '../assets/logo-wall/stripe.png';
 import Paypal from '../assets/logo-wall/paypal.png';
+import Bedc from '../assets/logo-wall/bedc.png';
+import MasterCard from '../assets/logo-wall/mastercard.png';
+import Mtn from '../assets/logo-wall/mtn.png';
+import Airtel from '../assets/logo-wall/airtel.png';
+import Payoneer from '../assets/logo-wall/payoneer.png';
+import Visa from '../assets/logo-wall/visa.png';
+import Mobile from '../assets/logo-wall/mobile.png';
+import Ikeja from '../assets/logo-wall/ikeja.png';
+import Dstv from '../assets/logo-wall/dstv.png';
 import LogoWall from "../common/LogoWall";
 
 function Partners() {
@@ -12,7 +21,36 @@ function Partners() {
           integrations with other payment platforms like stripe, paypal, payoneer and others</p>
       </article>
       <div className='xl:flex xl:justify-center'>
-        <LogoWall src1={Klarna} src2={Stripe} src3={Paypal} alt1="Klarna logo" alt2="Stripe logo" alt3="Paypal logo" />
+        <LogoWall
+          src1={Klarna}
+          src2={Stripe}
+          src3={Paypal}
+          alt1="Klarna logo"
+          alt2="Stripe logo"
+          alt3="Paypal logo" />
+        <LogoWall
+          src1={Bedc}
+          src2={MasterCard}
+          src3={Mtn}
+          alt1="Bedc logo"
+          alt2="MasterCard logo"
+          alt3="Mtn logo" />
+      </div>
+      <div className='xl:flex xl:justify-center'>
+        <LogoWall
+          src1={Airtel}
+          src2={Payoneer}
+          src3={Visa}
+          alt1="Airtel logo"
+          alt2="Payoneer logo"
+          alt3="Visa logo" />
+        <LogoWall
+          src1={Mobile}
+          src2={Ikeja}
+          src3={Dstv}
+          alt1="Mobile logo"
+          alt2="Ikeja logo"
+          alt3="Dstv logo" />
       </div>
     </section>
   )
