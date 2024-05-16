4d8ee5b86a213ab7b626d6870efe050499bde965
reynaroma <roma.reynamay@gmail.com>
2024-05-15 22:56:23 -0600

    Add a useState for the next and back function for the slides

--- src/common/Carousel.tsx
+++ src/common/Carousel.tsx
@@ -13,12 +13,28 @@ interface Slide {
 function Carousel({ slides }: CarouselProps) {
   let [current, setCurrent] = useState(0);
 
+  function previousSlide() {
+    if (current === 0) {
+      setCurrent(slides.length - 1);
+    } else {
+      setCurrent(current - 1);
+    }
+  }
+
+  function nextSlide() {
+    if (current === slides.length - 1) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
+  }
+
   return (
     <div className="relative h-[400px] max-w-[400px] overflow-hidden rounded-2xl">
       <div className="flex transition duration-300 ease-out" style={{transform: `translateX(-${current * 100}%)`}}>
         {slides.map((slide, index) => (
           <div key={index}>
-            {slide.name}
+            <img src={slide.src} alt={`Slideshow image ${index}`}/>
           </div>
         ))}
       </div>
