6f03b0f4d61c085fe51483f87cf4ba6d7b945579
reynaroma <roma.reynamay@gmail.com>
2024-05-15 21:06:39 -0600

    Finish editing the Features component

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,6 +1,7 @@
 import AppFrame from '../assets/app_frame.png';
 import ShieldCheck from '../assets/icons/check.png';
 import Coins from '../assets/icons/coins.png';
+import Frames from '../assets/frames.png';
 
 function Features() {
   return (
@@ -44,18 +45,18 @@ function Features() {
           </div>
           <h2 className='text-3xl font-semibold text-gray-800'>Cost reduction</h2>
           <p className='text-gray-500'>
-          Kobodrop reduced payments maintenance and processing fees. No hidden fees.
+            Kobodrop reduced payments maintenance and processing fees. No hidden fees.
           </p>
         </div>
-        <div className="flex flex-col rounded-2xl bg-sky-100 px-4 sm:px-0 md:flex-row md:gap-8 xl:w-2/3">
-          <div className="mt-10 flex flex-col justify-center gap-4 text-center sm:mx-10 md:mx-0 md:ml-10 md:w-1/2 md:text-left">
-            <h2 className="m-auto text-center text-3xl font-semibold text-gray-800 sm:w-[18ch] md:m-0 md:text-left">Pay with Kobodrop, quick, simple and easy</h2>
+        <div className="flex flex-col  gap-8 overflow-hidden rounded-2xl bg-sky-100 px-4 sm:px-0 md:flex-row md:gap-8 xl:w-2/3">
+          <div className="mt-10 flex flex-col justify-center gap-4 text-center sm:mx-10 md:mx-0 md:ml-10 md:w-1/2 md:text-left md:mt-0">
+            <h2 className="m-auto text-center text-3xl font-semibold text-gray-800 sm:w-[18ch] md:m-0 md:text-left">Send, receive and Exchange money</h2>
             <p className="m-auto text-center text-gray-500 sm:w-[34ch] md:m-0 md:text-left">
-              Paying your bills on Kobodrop has never been easier. Whether you are paying for electricity or internet, Kobodrop gets it done within seconds.
+              Transfers, payments all work on Kobodrop. Get your alert message immediately after a completed transaction.
             </p>
           </div>
-          <div className='m-auto mt-10 max-w-72 md:mx-10 md:w-1/2 lg:mx-0'>
-            <img src={AppFrame} alt='Kobodrop app frame' />
+          <div className='m-auto max-w-96 md:mx-10 md:w-1/2 lg:mx-0 md:mt-36'>
+            <img src={Frames} alt='Frame displaying logos of various payment solutions' />
           </div>
         </div>
 
