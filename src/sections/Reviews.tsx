954fdfac34e3364aa20b43b99aa25238b244953a
reynaroma <roma.reynamay@gmail.com>
2024-05-15 21:57:15 -0600

    Add constants on the Reviews component

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -1,12 +1,31 @@
 
 function Reviews() {
-  
+  const slides = [
+    {
+      src: "https://images.unsplash.com/photo-1595986630530-969786b19b4d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: '"Kobodrop has helped me send and receive money with little to no complications. It´s what everyone who wants to be on top of their money needs!"',
+      name: "Haruka Sato",
+      country: "Japan",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1554196038-950a8ab51827?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: '"Using Kobodrop has streamlined my financial transactions like never before. Highly recommended for anyone looking to simplify their finances!"',
+      name: "Elena Rodriguez",
+      country: "Spain",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1622556498246-755f44ca76f3?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: '"Kobodrop has been a game changer for my daily financial dealings. It allows for quick and secure money transfers, which saves me a lot of time and effort."',
+      name: "Thomas Morgan",
+      country: "USA",
+    }
+  ];
 
   return (
     <section id="reviews">
-      
+
     </section>
   )
 }
 
-export default Reviews
+export default Reviews;
