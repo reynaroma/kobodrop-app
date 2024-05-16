287c16689d82cb3174579bf31f6531d9584a2766
reynaroma <roma.reynamay@gmail.com>
2024-05-15 23:46:07 -0600

    Add the elements and style it

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,8 +1,18 @@
+import KobodropLight from '../assets/logos/kobodrop_logo_bright.png';
 
 function Footer() {
   return (
-    <section>
-      
+    <section className='bg-gray-800'>
+      <div className='flex max-w-7xl flex-col px-8 py-12 text-gray-300 lg:px-12 xl:m-auto'>
+        <div className='flex flex-col justify-between gap-12 md:flex-row md:gap-0'>
+          <div>
+            <img
+              className="mb-4 w-36"
+              src={KobodropLight}
+              alt="Light version of Kobodrop logo" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
