080f718a74bda96f4f44471751d7062dc0944ba6
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-16 15:46:51 -0600

    Add animation on the text; instead of using DisclosureButton, replace it with motion.a

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -57,14 +57,17 @@ function Nav() {
                 transition={{ duration: 0.3, ease: easeOut }}
                 className="flex flex-col items-center justify-center text-3xl gap-12 sm:hidden h-[calc(100vh-15vh)] text-white bg-gray-800">
                 {navLinks.map((link, index) => (
-                  <DisclosureButton
+                  <motion.a
                     className="block"
                     key={index}
-                    as="a"
                     href={link.href}
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.3, delay: index * 0.2, ease: "easeInOut" }}
                   >
                     {link.children}
-                  </DisclosureButton>
+                  </motion.a>
                 ))}
               </DisclosurePanel>
             )}
