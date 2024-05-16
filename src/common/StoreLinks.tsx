67fc11f947af35656705f74ffc935612aa05c984
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-16 16:12:44 -0600

    Add animation on the app store and google play button

--- src/common/StoreLinks.tsx
+++ src/common/StoreLinks.tsx
@@ -3,6 +3,7 @@ import AppleLogo from '../assets/logos/app_store.svg';
 import GoogleLogo from '../assets/logos/google_play.svg';
 import StoreLink from '../common/StoreLink';
 import GooglePlayBright from '../assets/logos/google_play_bright.svg';
+import { motion } from 'framer-motion';
 
 interface StoreLinksProps {
   type: BtnTypes;
@@ -16,25 +17,31 @@ export enum BtnTypes {
 function StoreLinks({ type }: StoreLinksProps) {
   if (type === BtnTypes.Standard) {
     return (
-      <div className='hidden mt-10 justify-center space-x-2 sm:flex md:justify-normal'>
-        <StoreLink
-          href="https://www.apple.com/app-store"
-          upperText='Download on the'
-          lowerText='App Store'
-          logo={AppleLogo}
-          className='flex gap-3 bg-zinc-900 px-4 py-3 rounded-lg
+      <div className='hidden mt-10 justify-center space-x-3 sm:flex md:justify-normal'>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <StoreLink
+            href="https://www.apple.com/app-store"
+            upperText='Download on the'
+            lowerText='App Store'
+            logo={AppleLogo}
+            className='flex gap-3 bg-zinc-900 px-4 py-3 rounded-lg
             text-white hover:bg-zinc-950 active:bg-zinc-800'
-          target='_blank'
-        />
-        <StoreLink
-          href="https://play.google.com/"
-          upperText='Get it on'
-          lowerText='Google Play'
-          logo={GoogleLogo}
-          className='flex gap-3 bg-zinc-900 px-4 py-3 rounded-lg
+            target='_blank'
+          />
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <StoreLink
+            href="https://play.google.com/"
+            upperText='Get it on'
+            lowerText='Google Play'
+            logo={GoogleLogo}
+            className='flex gap-3 bg-zinc-900 px-4 py-3 rounded-lg
             text-white hover:bg-zinc-950 active:bg-zinc-800'
-          target='_blank'
-        />
+            target='_blank'
+          />
+        </motion.div>
+
+
       </div>
     )
   }
