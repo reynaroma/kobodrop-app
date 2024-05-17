1f0a4d902b3ad573d8efe7f1a2c142564a7d1e4b
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-16 19:27:35 -0600

    Animate the logowall articles

--- src/common/LogoWall.tsx
+++ src/common/LogoWall.tsx
@@ -1,4 +1,5 @@
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
 
 export interface LogoWallProps {
   src1: string;
@@ -11,16 +12,32 @@ export interface LogoWallProps {
 
 function LogoWall({ src1, src2, src3, alt1, alt2, alt3 }:
   LogoWallProps) {
+
+  const logoWallRef = useRef(null);
+  const isLogoWallRefInView = useInView(logoWallRef, { once: true });
+
   return (
-    <div>
+    <div ref={logoWallRef}>
       <article className='flex mb-8 justify-center gap-8 text-center'>
-        <motion.div whileHover={{ scale: 1.05 }}>
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={isLogoWallRefInView ? { y: 0, opacity: 1 } : {}}
+          transition={{ delay: 0.3, duration: 0.8 }}
+          whileHover={{ scale: 1.05 }}>
           <img src={src1} alt={alt1} className='w-20 md:w-36' />
         </motion.div>
-        <motion.div whileHover={{ scale: 1.05 }}>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={isLogoWallRefInView ? { y: 0, opacity: 1 } : {}}
+          transition={{ delay: 0.3, duration: 0.8 }}
+          whileHover={{ scale: 1.05 }}>
           <img src={src2} alt={alt2} className='w-20 md:w-36' />
         </motion.div>
-        <motion.div whileHover={{ scale: 1.05 }}>
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={isLogoWallRefInView ? { y: 0, opacity: 1 } : {}}
+          transition={{ delay: 0.3, duration: 0.8 }}
+          whileHover={{ scale: 1.05 }}>
           <img src={src3} alt={alt3} className='w-20 md:w-36' />
         </motion.div>
       </article>
