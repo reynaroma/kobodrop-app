517a357cbc7170686161a318d6f3e27a44d9fc3f
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-16 18:59:23 -0600

    Animate the Feature when in view

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -2,21 +2,39 @@ import AppFrame from '../assets/app_frame.png';
 import ShieldCheck from '../assets/icons/check.png';
 import Coins from '../assets/icons/coins.png';
 import Frames from '../assets/frames.png';
+import { motion, useInView, useScroll } from 'framer-motion';
+import { useRef } from 'react';
 
 function Features() {
+
+  const featureRef = useRef(null);
+  const isFeatureRefInView = useInView(featureRef, { margin: "-100px" });
+
   return (
-    <section id="features"
+    <section id="features" ref={featureRef}
       className="flex max-w-7xl flex-col gap-10 px-10 pt-10 lg:px-12 xl:pt-20 m-auto">
       <article className="m-auto w-[30ch] text-center text-gray-500 md:m-0 md:w-full">
-        <h2 className="mb-4 text-4xl font-semibold text-gray-800">Make every penny count</h2>
-        <p>
+        <motion.h2
+          initial={{ y: -100, opacity: 0 }}
+          animate={isFeatureRefInView ? { y: 0, opacity: 1 } : {}}
+          transition={{ delay: 0.3, duration: 0.5 }}
+          className="mb-4 text-4xl font-semibold text-gray-800">Make every penny count</motion.h2>
+        <motion.p
+          initial={{ x: 100, opacity: 0 }}
+          animate={isFeatureRefInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ delay: 0.5, duration: 0.5 }}
+        >
           Spend smarter, lower your bills, get cashback on everything you buy, <br />
           and unlock credit to grow your business.
-        </p>
+        </motion.p>
 
       </article>
 
-      <article className="flex w-full flex-col gap-8 overflow-hidden xl:h-96 xl:flex-row">
+      <motion.article
+        initial={{ x: 100, opacity: 0 }}
+        animate={isFeatureRefInView ? { x: 0, opacity: 1 } : {}}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="flex w-full flex-col gap-8 overflow-hidden xl:h-96 xl:flex-row">
         <div className="flex flex-col rounded-2xl bg-sky-100 px-4 sm:px-0 md:flex-row md:gap-8 xl:w-2/3">
           <div className="mt-10 flex flex-col justify-center gap-4 text-center sm:mx-10 md:mx-0 md:ml-10 md:w-1/2 md:text-left">
             <h2 className="m-auto text-center text-3xl font-semibold text-gray-800 sm:w-[18ch] md:m-0 md:text-left">Pay with Kobodrop, quick, simple and easy</h2>
@@ -37,7 +55,7 @@ function Features() {
             Your money is 100% safe and secure on Kobodrop. No hassles, no glitches, get access to your money anytime.
           </p>
         </div>
-      </article>
+      </motion.article>
       <article className="flex w-full flex-col gap-8 xl:h-96 xl:flex-row">
         <div className='flex flex-col justify-center gap-4 rounded-2xl bg-orange-100 p-10 xl:w-1/3'>
           <div className='w-fit rounded-full bg-orange-200 p-4'>
